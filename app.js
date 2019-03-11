75dbf85c7c5d6590c66f3042ce671fcac8d879e4
Daniel Lee <chococloudy@gmail.com>
2019-03-10 22:53:05 -0600

    Functionally done

--- app.js
+++ app.js
@@ -18,18 +18,21 @@ let currentusernames = [];
 let socketIDs = [];
 let possibleUserName = ["Fu","Kyo","Kei","Gin","Kin","Oou","Gyo","Kya","Hi"];
 let lastUniqueUserName = 0;
-let user = {
-    "username": '',
-    "colour":'black',
-};
+
+// let user = {
+//     "username": '',
+//     "colour":'black',
+// };
+
+
 
 let chats=[];
 
-let chat = {
-    "timestamp":'',
-    "username":null,
-    "message":"",
-};
+// let chat = {
+//     "timestamp":'',
+//     "username":null,
+//     "message":"",
+// };
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/view/index.html');
@@ -37,14 +40,65 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    console.log("Socketid: " + socket.id);
-    if(socketIDs.includes(socket.id) === false){
-        socketIDs.push(socket.id);
-    }
-    console.log(socketIDs);
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+
+    let username = '';
+    socket.on('getCookie',function (cookie) {
+        // console.log("getCookie: " + cookie);
+
+        if(cookie === ''){
+            username = possibleUserName[lastUniqueUserName];
+            // console.log("no cookie, new ussername: " + username);
+            // console.log("username bool: " + users.includes(username));
+            while(currentusernames.includes(username)){
+                lastUniqueUserName++;
+                username = possibleUserName[lastUniqueUserName];
+                // console.log("generating new username: " + username);
+            }
+            currentusernames.push(username);
+            cookie = 'username=' + username;
+            socket.emit('setCookie',cookie);
+        }else{
+            let userregex = /username=(.*)/;
+            let match = userregex.exec(cookie);
+            // console.log("regex: " + match[1]);
+            username = match[1];
+            currentusernames.push(username);
+            // console.log(currentusernames);
+        }
+        let user = {};
+        user.username = username;
+        user.colour = "black";
+        users.push(user);
+        // console.log(users);
+        io.emit('userlist',currentusernames);
     });
+
+    socket.on('changeUsername',function (msg) {
+       let index = currentusernames.indexOf(msg.username);
+       currentusernames[index] = msg.newusername;
+       users[index] = msg.newusername;
+       username = msg.newusername;
+       let cookie = "username=" + username;
+       socket.emit('setCookie', cookie);
+       io.emit('userlist',currentusernames);
+    });
+
+
+
+    socket.on('disconnect', function(){
+        let index = currentusernames.indexOf(username);
+        if(index > -1){
+            currentusernames.splice(index,1);
+            users.splice(index,1);
+        }
+        console.log('user disconnected ' + username);
+        // console.log(users);
+        // console.log(currentusernames);
+        io.emit('userlist',currentusernames);
+    });
+    socket.emit('chat message',chats);
+
+
 
 });
 
@@ -52,51 +106,37 @@ io.on('connection', function(socket){
 
 io.on('connection',function(socket){
     socket.on('chat message', function (msg) {
-        console.log("message: " + msg);
-        io.emit('chat message',msg);
-    });
+        // console.log("Username: " + msg.username + " message: " + msg.content);
 
-    socket.on('getCookie',function (cookie) {
-        console.log("getCookie: " + cookie);
-        let username = '';
-        if(cookie === ''){
-            username = possibleUserName[lastUniqueUserName];
-            console.log("no cookie, new ussername: " + username);
-            console.log("username bool: " + users.includes(username));
-            while(currentusernames.includes(username)){
-                lastUniqueUserName++;
-                username = possibleUserName[lastUniqueUserName];
-                console.log("generating new username: " + username);
+        let date = new Date(Date.now());
+        let localeSpecificTime = date.toLocaleTimeString();
+        localeSpecificTime.replace(/:\d+ /, ' ');
+        // console.log("timestamp: " + localeSpecificTime);
+        let chat = {
+            "timestamp":'',
+            "username":null,
+            "message":"",
+            "colour":null
+        };
+        chat.timestamp = localeSpecificTime;
+        chat.username = msg.username;
+        chat.message = msg.content;
+        for(let i = 0; i < users.length; i++){
+            if(users[i].username === msg.username){
+                chat.colour = users[i].colour;
             }
-            cookie = 'username=' + username;
-            socket.emit('setCookie',cookie);
-        }else{
-            let userregex = /username=(.*)/;
-            let match = userregex.exec(cookie);
-            console.log("regex: " + match[1]);
-            username = match[1];
-            currentusernames.push(username);
-            console.log("bool: " + users.filter(users => (users.username === username)));
-            // while(users.filter(users => (users.username === username))){
-            //     console.log("Username: " + username);
-            //     username = possibleUserName[lastUniqueUserName];
-            //     lastUniqueUserName++;
-            // }
         }
-        let tempuser = user;
-        tempuser.username = username;
-        users.push(tempuser);
-        console.log(users);
+        chats.push(chat);
+        // console.log(chats);
+        io.emit('chat message',chats);
     });
 
 
-    socket.on('changeColour',function (command) {
-        
+    socket.on('changeColour',function (msg) {
+        let index = currentusernames.indexOf(msg.username);
+        users[index].colour = msg.colour;
     });
     
-    socket.on('changeUsername', function (command) {
-
-    });
 
 });
 
@@ -106,7 +146,3 @@ io.on('connection',function(socket){
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
-
-let userConnection = function () {
-
-};
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,15 +2,19 @@
 let message = '';
 let namechange = /\/nick\s([^\s]*)(.*)/;
 let colorchange = /\/nickcolor\s([^\s]*)(.*)/;
+let userregex = /username=(.*)/;
+let username = null;
 
 $(function (){
     let socket = io();
     //socket.emit('socketID',socket.id);
-    let cookietest = document.cookie;
-    console.log("socketid:" + cookietest);
+    let cookietemp = document.cookie;
+    console.log("socketid:" + cookietemp);
     socket.emit('getCookie',document.cookie);
-    if(cookietest !== ''){
-        //$('#UserNameLabel').text()
+    if(cookietemp !== ''){
+        let match = userregex.exec(cookietemp);
+        username = match[1];
+        $('#UserNameLabel').text("You are: " + username);
     }
     $('form').submit(function(e){
        e.preventDefault();
@@ -19,30 +23,61 @@ $(function (){
        let colourChangeMatch = colorchange.exec(message);
        if(nameChangeMatch){
            console.log("nameChange");
+           let msg = {
+             username: username,
+             newusername: nameChangeMatch[1]
+           };
+           socket.emit('changeUsername', msg);
        }
-       if(colourChangeMatch){
-           cosole.log("Colour change");
+       else if(colourChangeMatch){
+           console.log("Colour change");
+           let msg = {
+               username: username,
+               colour: colourChangeMatch[1]
+           };
+           socket.emit('changeColour',msg);
+       }else{
+           let msgsent ={};
+           msgsent.username = username;
+           msgsent.content = message;
+           socket.emit('chat message',msgsent);
        }
-       socket.emit('chat message',message);
+
+
        $('#message').val('');
+       $("#messages").scrollTop($("#messages")[0].scrollHeight);
        return false;
     });
-    socket.on('chat message', function (msg) {
-       $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', function (msgs) {
+        console.log(msgs);
+        $('#messages').empty();
+        for(let i = 0; i < msgs.length; i++){
+            let msgshown = "<li>"+ msgs[i].timestamp + " <span style='color: " + msgs[i].colour+ "'>"+ msgs[i].username + "</span> : <span " ;
+            if(username === msgs[i].username){
+                msgshown= msgshown + "style = 'font-weight: bold;'";
+            }
+            msgshown = msgshown + ">" + msgs[i].message + "</span>";
+            console.log(msgshown);
+            $('#messages').append(msgshown);
+        }
+
+       $("#messages").scrollTop($("#messages")[0].scrollHeight);
     });
     
     socket.on('setCookie',function (cookie) {
         document.cookie = cookie;
+        let match = userregex.exec(cookie);
+        username = match[1];
+        $('#UserNameLabel').text("You are: " + username);
     });
 
-
+    socket.on('userlist',function(ulist){
+        $('#currentUser').empty();
+        for(let i = 0; i < ulist.length; i++){
+            $("#currentUser").append("<li>" + ulist[i]);
+        }
+    });
 
 
 });
 
-let checkCommands = function(message){
-    let namechange = /\/nick\s([^\s]*)(.*)/;
-    let colorchange = /\/nickcolor\s([^\s]*)(.*)/;
-
-
-};
